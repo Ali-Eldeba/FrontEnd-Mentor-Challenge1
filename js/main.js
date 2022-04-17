28a37fb34e93ddd4ee59a6a6ee6b4aaac9af30f8
Ali Eldeba <alieldeba375@gmail.com>
2022-04-17 13:23:59 +0200

    fixing bugs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+/*
+  Looping on All of The images in The Website
+*/
+// Strting the Loop
+for (i = 0; i < document.images.length; i++) {
+  // images that have "draggable" Attribute
+  if (document.images[i].draggable === "") {
+    // images that have "draggable" Attribute and its Value is Empty
+    document.images[i].setAttribute("draggable", "false");
+  } else {
+    // Images Does not Have "draggable" Attribute
+    document.images[i].setAttribute("draggable", "false");
+  }
+}
+
 let body = document.querySelector("body");
 let barButton = document.querySelector(".bar");
 let closeButton = document.querySelector(".close");
@@ -26,6 +41,19 @@ function checkScroll() {
   if (mobileHeader.style.display == "block") {
     body.style.cssText = "max-height: 100vh; overflow: hidden; ";
   } else {
-    body.style.cssText = "max-height: fit-content; overflow: none; ";
+    body.style.cssText = "max-height: fit-content; overflow: none;";
   }
 }
+
+// Displaying none on resize
+window.onresize = function () {
+  if (window.innerWidth >= 768) {
+    closeButton.style.cssText = "display: none !important;";
+    barButton.style.cssText = "display: none !important;";
+    mobileHeader.style.cssText = "display: none !important;";
+  } else {
+    barButton.style.cssText = "display: block !important;";
+    closeButton.style.cssText = "display: none !important;";
+    mobileHeader.style.cssText = "display: none !important;";
+  }
+};
